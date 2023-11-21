b36f9740abba8290d85f2a90cebab6edb39d9f33
Gilbert Guszcza <guszczagilbert@gmail.com>
2023-11-21 16:25:24 +0100

    refactoring

--- src/UserWindow.java
+++ src/UserWindow.java
@@ -7,7 +7,7 @@ public class UserWindow extends JFrame{
     private JPanel Content;
     private JTextField textField1;
     private JButton button1;
-    private JPanel test;
+    private JPanel inv;
     private JButton button2;
     private JButton button3;
     private JPanel UserInterface;
