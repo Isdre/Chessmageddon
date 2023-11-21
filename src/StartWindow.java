b1800e4c2c72dc4953419a148336f7ab3d16b515
Gilbert Guszcza <guszczagilbert@gmail.com>
2023-11-21 01:25:48 +0100

    Logowanie, rejestracja - #początek

--- src/StartWindow.java
+++ src/StartWindow.java
@@ -1,16 +1,24 @@
+import chess_server_package.Client;
+import chess_server_package.MyListener;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
-public class StartWindow extends JFrame {
+
+public class StartWindow extends JFrame implements ActionListener {
+    private Client player;
     private JPanel Background;
     private JPanel Start;
     private JPanel Register;
     private JPanel Login;
+    //Log In
     private JPasswordField _login;
+    private JPasswordField _passwordLog;
+    //Sign In
     private JPasswordField _username;
     private JPasswordField _passwordSign;
-    private JPasswordField _passwordLog;
     private JCheckBox _remember;
     private JButton _buttonLog;
     private JButton _buttonSign;
@@ -22,15 +30,34 @@ public class StartWindow extends JFrame {
         setContentPane(Start);
         setSize(600,400);
         setVisible(true);
-        _buttonLog.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                loginPerformed();
-                new GameWindow();
-                dispose();
-            }
-        });
+        player = new Client((MyListener) this);
+        _buttonLog.addActionListener(this);
+        _buttonSign.addActionListener(this);
     }
-    public void loginPerformed() {
 
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == _buttonLog){
+            if (!loginPerformed()) return;
+        } else if (e.getSource() == _buttonSign) {
+            if (!signPerformed()) return;
+        }
+        new UserWindow(player);
+        dispose();
+    }
+
+    public boolean loginPerformed() {
+        try {
+            return  player.login(_login.getPassword().toString(), _passwordLog.getPassword().toString());
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public boolean signPerformed() {
+        try {
+            return  player.register(_username.getPassword().toString(), _passwordSign.getPassword().toString());
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
     }
 }
--- src/UserWindow.java
+++ src/UserWindow.java
@@ -1,5 +1,24 @@
+import chess_server_package.Client;
+
 import javax.swing.*;
 
-public class UserWindow {
-    private JLabel userLabel;
+public class UserWindow extends JFrame{
+    private Client player;
+    private JPanel Content;
+    private JTextField textField1;
+    private JButton button1;
+    private JPanel test;
+    private JButton button2;
+    private JButton button3;
+    private JPanel UserInterface;
+
+    public UserWindow(Client client){
+        super("TEST_1 Game");
+        player = client;
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        setContentPane(Content);
+        setSize(800,600);
+        setVisible(true);
+    }
 }
